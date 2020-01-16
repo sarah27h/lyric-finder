0f7b02ded9a5af87a953b09851d3e064e4a10c73
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2020-01-16 18:12:44 +0200

    Feat: mask api key

--- src/contexts/TrackContext.js
+++ src/contexts/TrackContext.js
@@ -4,10 +4,13 @@ export const TrackContext = createContext();
 
 const TrackContextProvider = props => {
   // state for tracks
-  const [tracks, setTracks] = useState([
-    { track: { track_name: 'abc' } },
-    { track: { track_name: '123' } }
-  ]);
+  const [tracks, setTracks] = useState([]);
+
+  const searchforLyric = tracks => {
+    console.log(tracks);
+  };
+
+  const API_KEY = process.env.REACT_APP_MUSICXMATCH_KEY;
 
   // state for heading
   const [heading, setHeading] = useState(['Top 10 Tracks']);
