65454730e826bba1af6d76c0726db1b81d2d43ac
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2020-02-14 18:47:54 +0200

    Fix: send fetch request for every keydown

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -2,8 +2,12 @@ import React, { Fragment, useState, useEffect, useContext } from 'react';
 import { TrackContext } from '../../contexts/TrackContext';
 
 const Search = () => {
+  // userInput to get user input all at once
+  // to avoid send request for every keydown
+  // update songTitle with the userInput after user click submit
+  // then send fetch request
+  const [userInput, setUserInput] = useState([]);
   const [songTitle, setSongTitle] = useState('');
-  const [fetchedTracks, setFetchedTracks] = useState([]);
   const { setTracks, setHeading } = useContext(TrackContext);
   const API_KEY = process.env.REACT_APP_MUSICXMATCH_KEY;
 
@@ -15,18 +19,20 @@ const Search = () => {
       `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${songTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${API_KEY}`
     )
       .then(response => response.json())
-      .then(data => setFetchedTracks(data.message.body.track_list))
+      .then(data => {
+        setHeading('Search Results');
+        setTracks(data.message.body.track_list);
+      })
       .catch(err => console.log(err));
   }, [songTitle]);
 
   const handleChange = e => {
-    setSongTitle(e.target.value);
+    setUserInput(e.target.value);
   };
   const handleSubmit = e => {
     e.preventDefault();
-    setHeading('Search Results');
-    setTracks(fetchedTracks);
-    setSongTitle('');
+    setSongTitle(userInput);
+    setUserInput('');
   };
   return (
     <Fragment>
@@ -36,7 +42,7 @@ const Search = () => {
         <input
           type="text"
           name="songTitle"
-          value={songTitle}
+          value={userInput}
           placeholder="song title ...."
           aria-label="song title"
           onChange={handleChange}
